51f636ebd249b43d1f7b7f78848a7aad8ddefac4
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2022-09-29 15:10:22 +0000

    Pending changes exported from your codespace

---
+++ src/component/counter.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import { ref } from 'vue'
+
+    let count = ref(0)
+</script>
+
+<template>
+    <h1>Hello vue</h1>
+    <button @click="count++">
+        count is: {{ count }}
+    </button>
+</template>
+
+<style></style>
