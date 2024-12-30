525a66691a27783c0556e7fdf1d023df932d453b
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2024-12-30 09:44:00 +0100

    setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,34 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        incognito: ["var(--incognito)"],
+        inter: ["var(--inter)"],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "primary-color": "#fbbf24",
+        "secondary-color": "#f59e0b",
+        "tertiary-color": "#ea580c",
+        "primary-bg": "rgba(39, 39, 43, 0.4)",
+        "secondary-bg": "rgba(250, 250, 250, 0.4)",
+      },
+      boxShadow: {
+        "line-light": "rgba(17, 17, 26, 0.1) 0px 1px 0px",
+        "line-dark": "rgb(29, 29, 32) 0px 1px 0px",
+      },
+      gridTemplateColumns: {
+        custom: "1.2fr 1fr",
+      },
+      gridTemplateRows: {
+        fit: "min-content 0fr",
+        full: "min-content 1fr",
+      },
+      backgroundImage: {
+        noise:
+          "url('/noise.png')",
+      },
+      backgroundPosition: {
+        zero: "0 0",
       },
     },
   },
