04b899e1a881d5d52b2ffb2fc92f58477f976a1a
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2024-12-31 09:01:41 +0100

    UI: minor update

--- app/components/pages/GithubCalendarComponent.tsx
+++ app/components/pages/GithubCalendarComponent.tsx
@@ -1,13 +1,25 @@
+import Link from "next/link";
 import { Slide } from "../shared/Slide";
 import ContributionGraph from "./ContributionGraph";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 export default function GithubCalendarComponent() {
   return (
     <section>
-      <Slide delay={0.16} className="">
+      <Slide
+        delay={0.16}
+        className="flex items-center justify-between mb-10 w-[60rem]"
+      >
         <h2 className="font-incognito text-4xl font-bold tracking-tight">
           Contrbution Graph
         </h2>
+
+        <Link
+          href="/projects"
+          className="flex hover:text-primary-color hover:underline text-lg items-center gap-1 font-incognito"
+        >
+          Veiw Projects <MdKeyboardArrowRight />{" "}
+        </Link>
       </Slide>
 
       <Slide delay={0.18}>
