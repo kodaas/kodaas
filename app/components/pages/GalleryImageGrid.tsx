bf1191ce28cc1aaa043679adf3beb0f83799ad18
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2025-01-09 03:53:22 +0100

    fix: urgent bug bug

--- app/components/pages/GalleryImageGrid.tsx
+++ app/components/pages/GalleryImageGrid.tsx
@@ -11,18 +11,18 @@ export function GalleryImageGrid() {
   );
 }
 
-function TestData(
-  amount: number,
-): Array<{ url: string; width: number; height: number }> {
-  const dimentions = [250, 300, 350, 400, 450];
+// function TestData(
+//   amount: number,
+// ): Array<{ url: string; width: number; height: number }> {
+//   const dimentions = [250, 300, 350, 400, 450];
 
-  return new Array(amount).fill(0).map(() => {
-    const width = dimentions[Math.floor(Math.random() * dimentions.length)];
-    const height = dimentions[Math.floor(Math.random() * dimentions.length)];
-    return {
-      url: `https://placehold.co/${width}x${height}.png`,
-      width,
-      height,
-    };
-  });
-}
+//   return new Array(amount).fill(0).map(() => {
+//     const width = dimentions[Math.floor(Math.random() * dimentions.length)];
+//     const height = dimentions[Math.floor(Math.random() * dimentions.length)];
+//     return {
+//       url: `https://placehold.co/${width}x${height}.png`,
+//       width,
+//       height,
+//     };
+//   });
+// }
